ef04a4c9b4374524c61136495113d0f580cb484f
Jacky Tang <jackytck@gmail.com>
2018-01-26 23:17:54 +0800

    :hankey: example gas required

--- hello.sol
+++ hello.sol
@@ -10,4 +10,12 @@ contract Inbox {
     function setMessage(string newMessage) public {
         message = newMessage;
     }
+
+    /* // total 14 gas
+    function doMath(int a, int b) {
+      a + b; // 3 gas
+      b - a; // 3 gas
+      a * b; // 5 gas
+      a == 0; // 3 gas
+    } */
 }
